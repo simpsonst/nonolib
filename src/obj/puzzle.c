54b01ada898b9d5abc23d84ae8e6a9982712ca6d
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2022-06-26 22:15:15 +0100

    Fixed contact details.

--- src/obj/puzzle.c
+++ src/obj/puzzle.c
@@ -16,7 +16,7 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  Contact Steven Simpson <ss@comp.lancs.ac.uk>
+ *  Contact Steven Simpson <https://github.com/simpsonst>
  */
 
 #include <stdlib.h>
